24ec35edd0e6ab75de12d99685dc259051e697e4
ricoberger <mail@ricoberger.de>
2025-03-17 20:47:12 +0100

    Add Cloudflare Worker for Analytics

---
+++ analytics/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS "visitors";
+
+CREATE TABLE IF NOT EXISTS "visitors" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "url" TEXT DEFAULT NULL,
+  "referer" TEXT DEFAULT NULL,
+  "user_agent" TEXT DEFAULT NULL,
+  "city" TEXT DEFAULT NULL,
+  "continent" TEXT DEFAULT NULL,
+  "country" TEXT DEFAULT NULL,
+  "latitude" DECIMAL DEFAULT NULL,
+  "longitude" DECIMAL DEFAULT NULL,
+  "postal_code" TEXT DEFAULT NULL,
+  "region" TEXT DEFAULT NULL,
+  "region_code" TEXT DEFAULT NULL,
+  "timezone" TEXT DEFAULT NULL,
+  "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP
+);
---
+++ analytics/src/index.js
@@ -0,0 +1,70 @@
+export default {
+  async fetch(request, env, ctx) {
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+    };
+
+    try {
+      if (request.method === "POST") {
+        const userAgent = request.headers.get("user-agent");
+        const referer = request.headers.get("referer");
+        const city = request.cf.city;
+        const continent = request.cf.continent;
+        const country = request.cf.country;
+        const latitude = request.cf.latitude;
+        const longitude = request.cf.longitude;
+        const postalCode = request.cf.postalCode;
+        const region = request.cf.region;
+        const regionCode = request.cf.regionCode;
+        const timezone = request.cf.timezone;
+
+        const body = await request.json();
+
+        const { results } = await env.DB.prepare(
+          "INSERT INTO visitors (url, referer, user_agent, city, continent, country, latitude, longitude, postal_code, region, region_code, timezone) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12)",
+        )
+          .bind(
+            body.url,
+            referer,
+            userAgent,
+            city,
+            continent,
+            country,
+            latitude,
+            longitude,
+            postalCode,
+            region,
+            regionCode,
+            timezone,
+          )
+          .all();
+
+        return Response.json(results, {
+          headers: {
+            ...corsHeaders,
+          },
+        });
+      }
+
+      return Response.json(
+        { status: "success" },
+        {
+          headers: {
+            ...corsHeaders,
+          },
+        },
+      );
+    } catch (err) {
+      return Response.json(
+        { status: "error" },
+        {
+          headers: {
+            ...corsHeaders,
+          },
+        },
+      );
+    }
+  },
+};
